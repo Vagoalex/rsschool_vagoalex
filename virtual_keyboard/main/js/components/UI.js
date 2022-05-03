1ff3167c101c3b8b4c3b6ca444a33cf55073ad30
Vago Aleksandr <vgoalex666@mail.ru>
2022-05-03 19:22:08 +0300

    init: created a virtual keyboard in HTML and CSS

---
+++ virtual_keyboard/main/js/components/UI.js
@@ -0,0 +1,14 @@
+const UI = {
+	keys: document.querySelectorAll('.keys'),
+	spaceKey: document.querySelector('.space_key'),
+	shift_left: document.querySelector('.shift_left'),
+	shift_right: document.querySelector('.shift_right'),
+	caps_lock_key: document.querySelector('.caps-lock_key'),
+	toggle_circle: document.querySelector('.toggle_circle'),
+	night_mode: document.querySelector('.night_mode'),
+	body: document.querySelector('body'),
+	text_input: document.querySelector('.text'),
+	change_color: document.querySelector('change_light-color'),
+	colors: document.querySelector('colors__input'),
+	keyboard_wrapper: document.querySelector('keyboard-wrapper'),
+};
