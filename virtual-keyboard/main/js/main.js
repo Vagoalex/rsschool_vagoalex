df2e1d65a346b4af36e41fd0ec5f055a742a236d
Vago Aleksandr <vgoalex666@mail.ru>
2022-05-07 13:05:16 +0300

    feat: JS: init method is almost done. I need to made a keydown and keyup methods. CreateKeys almost done.

--- virtual-keyboard/main/js/main.js
+++ virtual-keyboard/main/js/main.js
@@ -9,7 +9,7 @@ export function createWindowElements() {
 					<source src="../assets/cyberbg.mp4"  type="video/mp4"></source>
 				</video>
 			</div>
-			<div class="video-mute">
+			<div id="videoMute" class="video-mute">
 			<button class="video-mute__btn">turn on music</button>
 			</div>
 			<div class="wrapper-text">
@@ -23,7 +23,7 @@ export function createWindowElements() {
 const Keyboard = {
 	elements: {
 		main: null,
-		keysContainer: null,
+		keyboardKeys: null,
 		keys: [],
 	},
 	eventHandlers: {
@@ -33,16 +33,185 @@ const Keyboard = {
 	props: {
 		value: '',
 		capsLock: false,
+		shift: false,
+		EnRu: false,
 	},
 
 	init() {
 		// create main elements
 		this.elements.main = document.createElement('div');
-		console.log('Init');
+		this.elements.keyboardKeys = document.createElement('div');
+
+		// settings main elements
+		this.elements.main.classList.add('keyboard', 'keyboard--hidden');
+		this.elements.keyboardKeys.classList.add('keyboard__keys');
+		this.elements.keyboardKeys.appendChild(this.createKeys());
+
+		this.elements.keys = this.elements.keyboardKeys.querySelectorAll('.key');
+
+		// add to DOM tree
+		this.elements.main.appendChild(this.elements.keyboardKeys);
+		const container = document.querySelector('.container');
+		const textarea = document.querySelector('.wrapper-text');
+
+		// added [this.elements.main] before [textarea]
+		container.insertBefore(this.elements.main, textarea);
+
+		// automatically use keyboard for elements with textarea in class .text
+		document.querySelectorAll('.text').forEach(element => {
+			element.addEventListener('focus', () => {
+				this.open(element.value, currentValue => {
+					element.value = currentValue;
+				});
+			});
+		});
+
+		const body = document.querySelector('body');
+		// future event for keydown
+		// future event for keyup
 	},
 
 	createKeys() {
-		// create keys elements
+		const keysFragment = document.createDocumentFragment();
+		const keyLayout = [
+			'`',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'6',
+			'7',
+			'8',
+			'9',
+			'0',
+			'-',
+			'=',
+			'Backspace',
+			'Tab',
+			'q',
+			'w',
+			'e',
+			'r',
+			't',
+			'y',
+			'u',
+			'i',
+			'o',
+			'p',
+			'[',
+			']',
+			'\\',
+			'Caps Lock',
+			'a',
+			's',
+			'd',
+			'f',
+			'g',
+			'h',
+			'j',
+			'k',
+			'l',
+			';',
+			"'",
+			'Enter',
+			'Shift',
+			'z',
+			'x',
+			'c',
+			'v',
+			'b',
+			'n',
+			'm',
+			',',
+			'.',
+			'/',
+			'Shift',
+			'Up',
+			'Eng/Rus',
+			'Ctrl',
+			'Done',
+			'Alt',
+			'Space',
+			'Alt',
+			'Ctrl',
+			'Left',
+			'Bottom',
+			'Right',
+		];
+
+		keyLayout.forEach(key => {
+			const keyElem = document.createElement('button');
+			keyElem.classList.add('key');
+			keyElem.setAttribute('type', 'button');
+
+			switch (key) {
+				case 'Backspace':
+					keyElem.classList.add('key--special');
+					keyElem.textContent = 'Backspace';
+
+					keyElem.addEventListener('click', () => {
+						const textarea = document.querySelector('.textarea');
+						const position = document.querySelector('.textarea').selectionStart;
+						const first = this.props.value.slice(0, position);
+						const second = this.props.value.slice(position);
+
+						textarea.focus();
+						this.props.value = first.substring(0, first.length - 1) + second;
+						this.triggerEventBack('oninput');
+					});
+					break;
+				case 'Caps Lock':
+					break;
+				case 'Enter':
+					break;
+				case 'Space':
+					break;
+				case 'Shift':
+					break;
+				case 'Eng/Rus':
+					break;
+				case 'Ctrl':
+					break;
+				case 'Done':
+					break;
+				case 'Up':
+					break;
+				case 'Left':
+					break;
+				case 'Right':
+					break;
+				case 'Bottom':
+					break;
+
+				default:
+					// create standart key
+					keyElem.textContent = key;
+
+					// create event on 'click' in key
+					keyElem.addEventListener('click', () => {
+						const textarea = document.querySelector('.text');
+						const position = document.querySelector('.text').selectionStart;
+						const first = this.props.value.slice(0, position);
+						const second = this.props.value.slice(position);
+
+						textarea.focus();
+						if (this.props.capsLock && this.props.shift) {
+							this.props.value = first + keyElem.textContent.toLowerCase() + second;
+						} else if (this.props.shift) {
+							this.props.value = first + keyElem.textContent.toUpperCase() + second;
+						} else if (this.props.capsLock) {
+							this.props.value = first + keyElem.textContent.toUpperCase() + second;
+						} else {
+							this.props.value = first + keyElem.textContent.toLowerCase() + second;
+						}
+						this.defaultSound();
+
+						this.triggerEvent('oninput');
+					});
+					break;
+			}
+		});
 	},
 
 	triggerEvent(handlerName) {
